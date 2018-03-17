8a82294422e679d98b7068688671eb875b9fb7a1
ose <oserambonimazato@gmail.com>
2018-03-17 13:31:30 +0300

    Initial commit

---
+++ src/org/admin/dao/DerogationDao.java
@@ -0,0 +1,19 @@
+package org.admin.dao;
+
+import org.admin.dao.DaoException;
+
+import java.util.List;
+
+
+
+import org.admin.beans.Derogation;
+
+public interface DerogationDao
+{
+	
+	List<Derogation> getAllDerogation()throws DaoException;
+    int countEtudiantDerogatedPerPortail(String choix) throws DaoException;
+    List<Derogation> getAllDerogationPerPortail(String choix)throws DaoException;
+    /*int countEtudiantConfirmedPerPortail(String choix) throws DaoException;
+	int countEtudiantSubsribedPerPortail(String choix) throws DaoException;*/
+}
